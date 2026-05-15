72e48233c14d562ffad45089e06486614156bf0d
Abhishek Keshav <abhishekkeshav@Abhisheks-MacBook-Air.local>
2026-05-15 18:09:17 +0530

    Initial Xcode project setup with Core Data enabled

---
+++ MatchMate/MatchMate/MatchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchMateApp.swift
+//  MatchMate
+//
+//  Created by Abhishek Keshav on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MatchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
